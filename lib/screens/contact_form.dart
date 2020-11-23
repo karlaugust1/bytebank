33054adbac16a2337d67a7cdf7ea30c701a4c5ca
Karl Harder <karl.harder@roit.com.br>
2020-11-23 12:52:49 -0300

    feat: add sqflite integration

--- lib/screens/contact_form.dart
+++ lib/screens/contact_form.dart
@@ -1,4 +1,4 @@
-import 'package:bytebank/database/app_database.dart';
+ import 'package:bytebank/database/dao/contact_dao.dart';
 import 'package:bytebank/models/contact.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +12,8 @@ class _ContactFormState extends State<ContactForm> {
   final TextEditingController _nameController = TextEditingController();
 
   final TextEditingController _accountNumberController = TextEditingController();
+
+  final ContactDao _dao = ContactDao();
 
   @override
   Widget build(BuildContext context) {
@@ -53,7 +55,7 @@ class _ContactFormState extends State<ContactForm> {
 
                     final Contact newContact = new Contact(0, name, accountNumber);
 
-                    save(newContact).then((id) => Navigator.pop(context));
+                    _dao.save(newContact).then((id) => Navigator.pop(context));
                   },
                 ),
               ),
